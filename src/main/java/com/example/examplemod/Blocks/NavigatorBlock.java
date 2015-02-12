c2c3ffc44b7cb4cf54d8c111e34bfe8823419d4c
Markus Poggenburg <s_id2f3e@uni-bremen.de>
2015-02-12 15:55:41 +0100

    Initial

---
+++ src/main/java/com/example/examplemod/Blocks/NavigatorBlock.java
@@ -0,0 +1,5 @@
+package com.example.examplemod.Blocks;
+
+public class NavigatorBlock {
+
+}
---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,35 @@
+package com.example.examplemod;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    public static Item commandBlock = new Block
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    	
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+    	
+    }
+}
