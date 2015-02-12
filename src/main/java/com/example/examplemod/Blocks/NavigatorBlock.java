2b5047de7317086ef4df4575ac68217862304308
Markus Poggenburg <s_id2f3e@uni-bremen.de>
2015-02-12 16:03:19 +0100

    Minor fixes

--- src/main/java/com/example/examplemod/Blocks/NavigatorBlock.java
+++ src/main/java/com/example/examplemod/Blocks/NavigatorBlock.java
@@ -1,5 +1,13 @@
 package com.example.examplemod.Blocks;
 
-public class NavigatorBlock {
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 
+public class NavigatorBlock extends Item{
+
+	
+	public NavigatorBlock(int par1, String text) {
+		
+		this.setCreativeTab(CreativeTabs.tabMisc);
+	}
 }
--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,5 +1,7 @@
 package com.example.examplemod;
 
+import com.example.examplemod.Blocks.NavigatorBlock;
+
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
@@ -14,7 +16,9 @@ public class ExampleMod
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
     
-    public static Item commandBlock = new Block
+    private static int navigatorBlockId = 1000;
+    
+    public static Item commandBlock = new NavigatorBlock(navigatorBlockId, "NavigatorBlock");
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
