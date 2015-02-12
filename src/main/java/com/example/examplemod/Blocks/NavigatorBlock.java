892a85ce7957a2f7b8f387bed5d3fca3fd5c0967
Markus Poggenburg <s_id2f3e@uni-bremen.de>
2015-02-12 16:25:55 +0100

    UTF-8 Fail fixes

--- src/main/java/com/example/examplemod/Blocks/NavigatorBlock.java
+++ src/main/java/com/example/examplemod/Blocks/NavigatorBlock.java
@@ -6,19 +6,21 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
-public class NavigatorBlock extends Item{
+public class NavigatorBlock extends Item {
 
-	
-	public NavigatorBlock(int par1, String text) {
-		
-		this.setCreativeTab(CreativeTabs.tabMisc);
-		this.setMaxStackSize(1);
-	}
-	
-	@Override
-	public ItemStack onItemRightClick(ItemStack arg0, World arg1, EntityPlayer arg2) {
-		return super.onItemRightClick(arg0, arg1, arg2);
-		
-		//Console öffnenen und auf tolle mineSpace Befehle warten.
-	}
+    public NavigatorBlock(int par1, String text) {
+
+        this.setCreativeTab(CreativeTabs.tabMisc);
+        this.setMaxStackSize(1);
+    }
+
+
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack arg0, World arg1,
+            EntityPlayer arg2) {
+        return super.onItemRightClick(arg0, arg1, arg2);
+
+        // Console Ã¶ffnenen und auf tolle mineSpace Befehle warten.
+    }
 }
