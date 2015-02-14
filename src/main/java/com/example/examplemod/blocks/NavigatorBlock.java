e56a3d4f39d046e5255b67c41b78088b1c688dc9
Arved Friedemann <arved@uni-bremen.de>
2015-02-14 16:21:41 +0100

    Made a Method to send a chat message to the Player when he clicks the block

--- src/main/java/com/example/examplemod/blocks/NavigatorBlock.java
+++ src/main/java/com/example/examplemod/blocks/NavigatorBlock.java
@@ -2,10 +2,18 @@ package com.example.examplemod.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.IChatComponent;
 import net.minecraft.world.World;
 
 public class NavigatorBlock extends Block {
@@ -17,13 +25,18 @@ public class NavigatorBlock extends Block {
     }
 
 
-    /*
+    
     @Override
-    public ItemStack onItemRightClick(ItemStack arg0, World arg1,
-            EntityPlayer arg2) {
-        return super.onItemRightClick(arg0, arg1, arg2);
-
-        // Console öffnenen und auf tolle mineSpace Befehle warten.
-    }
-    */
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ) {
+    	 if (worldIn.isRemote)
+         {
+             return true;
+         }
+         else
+         {
+        	 ChatComponentText text = new ChatComponentText("Yay, you clicked our block!");
+			 playerIn.addChatComponentMessage(text);
+        	 return true;
+         }
+    }    
 }
