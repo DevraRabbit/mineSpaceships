143b6e43e5356fd8943f5e6838cf4481b22891e9
Jannes Höke <jhoeke@uni-bremen.de>
2015-02-16 00:30:44 +0100

    Changed a few comments

--- src/main/java/com/example/examplemod/blocks/NavigatorBlock.java
+++ src/main/java/com/example/examplemod/blocks/NavigatorBlock.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class NavigatorBlock extends Block implements ITileEntityProvider{
 	/**
 	 * Block to implement an interface for the Spaceships
-	 * @param name
+	 * @param name The display name of the block
 	 */
     public NavigatorBlock(String name) {
     	//Make a Block with certain attributes. Sponge for example is a quite soft block. Does not affect
@@ -40,13 +40,13 @@ public class NavigatorBlock extends Block implements ITileEntityProvider{
      */
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ) {    	 
-    	//Check wether this code is executed on the client. It is sometimes important to check wether
+    	//Check whether this code is executed on the client. It is sometimes important to check whether
     	//we do things locally or on the server. In this case we act on the client.
     	if(worldIn.isRemote){
-    		//Send the player a message. This is the common way of sending texts to a single player.
+    		//Send the player a message. This is the common way of sending texts to a single player using the chat.
 			 ChatComponentText text = new ChatComponentText("Opening Console");
 			 playerIn.addChatComponentMessage(text);	 
-			 //Get the tileentity from the world, meaning we get the object that is associated to the position
+			 //Get the TileEntity from the world, which is the object that is associated to the position
 			 //of our block and that can execute extra code. In our example this is opening a console.
 			 TileEntity entity = worldIn.getTileEntity(pos);
 			 if(entity instanceof ChatRegisterEntity){
@@ -54,11 +54,11 @@ public class NavigatorBlock extends Block implements ITileEntityProvider{
 				 ((ChatRegisterEntity)entity).Activate(playerIn);
 			 }			 
     	 }
-    	 //returns wether a block will not be placed next to it when clicked
+    	 //returns true to prevent placing a block (which would be the default behavior for rightclicking)
     	 return true;  
     }
     /**
-     * This gives the TileEntity associated with this block. This way Minecraft can register it into the world
+     * @returns the TileEntity associated with this block. This way Minecraft can register it into the world
      */
 	@Override
 	public TileEntity createNewTileEntity(World worldIn, int meta) {
