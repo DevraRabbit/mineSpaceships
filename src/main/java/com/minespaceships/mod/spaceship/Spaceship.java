da4262717f1f84041f0aa68b732d6c0f609f0411
Arved Friedemann <arved@uni-bremen.de>
2015-02-17 16:05:28 +0100

    Made more Vectoroperations

--- src/main/java/com/minespaceships/mod/spaceship/Spaceship.java
+++ src/main/java/com/minespaceships/mod/spaceship/Spaceship.java
@@ -5,36 +5,32 @@ import javax.vecmath.Vector3d;
 import net.minecraft.util.Vec3;
 
 public class Spaceship {
-	private Vector3d minPosition;
-	private Vector3d maxPosition;
-	private Vector3d span;
-	private Vector3d origin;
+	private Vec3 minPosition;
+	private Vec3 maxPosition;
+	private Vec3 span;
+	private Vec3 origin;
 	
-	public Spaceship(Vector3d minPosition, Vector3d maxPosition){
+	public Spaceship(final Vec3 minPosition,final Vec3 maxPosition){
 		setMeasurements(minPosition, maxPosition);
 	}
-	public Spaceship(Vector3d minPosition, double dimX, double dimY, double dimZ){
-		Vector3d recSpan = new Vector3d(dimX, dimY, dimZ);
-		recSpan.add(minPosition);
-		setMeasurements(minPosition, recSpan);
+	public Spaceship(final Vec3 minPosition, final double dimX, final double dimY, final double dimZ){
+		Vec3 recSpan = new Vec3(dimX, dimY, dimZ);
+		setMeasurements(minPosition, recSpan.add(minPosition));
 	}
-	public Spaceship(Vector3d minSpan, Vector3d origin, Vector3d maxSpan){
-		Vector3d minPos = (Vector3d) minSpan.clone();
-		minPos.add(origin);
-		Vector3d maxPos = (Vector3d) maxSpan.clone();
-		maxPos.add(origin);
-		setMeasurements(minPos, maxPos);
-		this.origin = (Vector3d) origin.clone();
+	public Spaceship(final Vec3 minSpan, final Vec3 origin, final Vec3 maxSpan){
+		setMeasurements(minSpan.add(origin), maxSpan.add(origin));
+		this.origin = origin;
 	}
 	
-	private void setMeasurements(Vector3d minPos, Vector3d maxPos){
-		minPosition = (Vector3d) minPos.clone();
-		maxPosition = (Vector3d) maxPos.clone();
-		span = (Vector3d) maxPosition.clone();
-		span.sub(minPosition);
-		origin = (Vector3d)maxPosition.clone();
+	private void setMeasurements(final Vec3 minPos, final Vec3 maxPos){
+		minPosition = minPos;
+		maxPosition = maxPos;
+		span = maxPos.subtract(minPos);
+		origin = ;
 		Vector3d halfDist = (Vector3d)span.clone();
 		halfDist.scale(1/2);
 		origin.add(halfDist);
 	}
+	
+	
 }
