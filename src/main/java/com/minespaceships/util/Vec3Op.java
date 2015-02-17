e91aab8ea1bdede6e8b4b664fb3e80d04c7432ec
Arved Friedemann <arved@uni-bremen.de>
2015-02-17 16:46:59 +0100

    Made other vector type

--- src/main/java/com/minespaceships/util/Vec3Op.java
+++ src/main/java/com/minespaceships/util/Vec3Op.java
@@ -1,9 +1,13 @@
 package com.minespaceships.util;
 
 import net.minecraft.util.Vec3;
+import net.minecraft.util.Vec3i;
 
 public class Vec3Op{
 	public static Vec3 scale(Vec3 vec, double ratio){
 		return new Vec3(vec.xCoord * ratio, vec.yCoord * ratio, vec.zCoord * ratio);
+	}
+	public static Vec3i scale(Vec3i vec, double ratio){
+		return new Vec3i((double)vec.getX() * ratio, (double)vec.getY() * ratio, (double)vec.getY() * ratio);
 	}
 }
