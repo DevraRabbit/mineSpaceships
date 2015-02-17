038ed3642ecfdce7b9c4c20230fcb16940ec3322
Jannes Höke <jhoeke@uni-bremen.de>
2015-02-17 17:59:24 +0100

    moved functionality to static methods in SpaceshipCommands and added attribute 'Spaceship ship'

--- src/main/java/com/minespaceships/mod/overhead/ChatRegisterEntity.java
+++ src/main/java/com/minespaceships/mod/overhead/ChatRegisterEntity.java
@@ -3,7 +3,9 @@ package com.minespaceships.mod.overhead;
 import java.util.regex.*;
 
 import com.minespaceships.mod.spaceship.Spaceship;
+import com.minespaceships.mod.spaceship.SpaceshipCommands;
 import com.minespaceships.util.BlockCopier;
+import com.minespaceships.util.Calculator;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiChat;
@@ -19,6 +21,10 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.Side;
 
 public class ChatRegisterEntity extends TileEntity {
+	
+	//Attributes
+	private Spaceship ship;
+	
 	/**
 	 * Activates the TileEntity and opens a custom chat to the player
 	 * @param player
@@ -50,107 +56,19 @@ public class ChatRegisterEntity extends TileEntity {
 			player.addChatComponentMessage(new ChatComponentText("I love you!"));
 		//Define the 'calc' command, which parses a math expression
 		} else if(command.startsWith("calc")) {
-			//prepare the math expression
-			command = command.substring("calc".length()).replaceAll("\\s", ""); 
-			try {
-				player.addChatComponentMessage(new ChatComponentText(command + " = " + solve(command)));
-			} catch (Exception ex) {
-				player.addChatComponentMessage(new ChatComponentText("calc: Error processing intput"));
-			}
+			Calculator.calc(command, player);
+		} else if (command.startsWith("init")) {
+			SpaceshipCommands.init(command, worldObj, this, player, ship);
 		} else if (command.startsWith("move")) {
-			command = command.substring("move".length()).replaceAll("\\s", "");
-
-			Pattern poffset = Pattern.compile("([\\-\\+]?[0-9]+);([\\-\\+]?[0-9]+);([\\-\\+]?[0-9]+)");
-			Matcher moffset = poffset.matcher(command);
-			if(moffset.matches()) {
-				BlockPos vec_move = new BlockPos(Integer.valueOf(moffset.group(1)), Integer.valueOf(moffset.group(2)), Integer.valueOf(moffset.group(3)));
-				
-				Spaceship s = new Spaceship(new BlockPos(-5, -5, -5), this.pos, new BlockPos(5, 5, 5), worldObj);
-				s.copyTo(vec_move);
-				player.addChatComponentMessage(new ChatComponentText("Moving by " + moffset.group(1) + "; " + moffset.group(2) + "; " + moffset.group(3)));
-			} else {
-				player.addChatComponentMessage(new ChatComponentText("move: Error processing intput"));
-			}
+			SpaceshipCommands.move(command, worldObj, this, player, ship);
 		}
 	}
 
+	public void setShip(Spaceship ship) {
+		this.ship = ship;
+	}
 	
-	//Attributes for math parser
-	private Pattern brackets = Pattern.compile("\\(.*?\\)");
-	private Pattern times = Pattern.compile("(\\-?[0-9\\.]+)\\*(\\-?[0-9\\.]+)");
-	private Pattern div = Pattern.compile("(\\-?[0-9\\.]+)/(\\-?[0-9\\.]+)");
-	private Pattern plus = Pattern.compile("(\\-?[0-9\\.]+)\\+(\\-?[0-9\\.]+)");
-	private Pattern minus = Pattern.compile("(\\-?[0-9\\.]+)\\-(\\-?[0-9\\.]+)");
-	private Pattern done = Pattern.compile("\\-?[0-9\\.]+");
-	private Matcher mb, mt, md, mp, mm, mdone;
-	
-	//Recursively parses a String containing a math expression
-	//Supported are brackets, +-*/, negative numbers and doubles 
-	private String solve(String expr) throws Exception {
-		mb = brackets.matcher(expr);
-		mt = times.matcher(expr);
-		md = div.matcher(expr);
-		mp = plus.matcher(expr);
-		mm = minus.matcher(expr);
-		mdone = done.matcher(expr);
-		
-		if(mdone.matches()) return expr;
-		
-		if(mb.find()) {
-			Matcher m = mb;
-			String before = expr.substring(0, m.start());
-			String inner = expr.substring(m.start() + 1, m.end() - 1);
-			String after = expr.substring(m.end());			
-			return solve(before + solve(inner) + after);
-		} else if (mt.find()) {
-			Matcher m = mt;
-			String before = expr.substring(0, m.start());
-			
-			String first = expr.substring(m.start(1), m.end(1));
-			String second = expr.substring(m.start(2), m.end(2));
-			
-			String inner = String.valueOf(Double.valueOf(first) * Double.valueOf(second));
-			
-			String after = expr.substring(m.end());
-			
-			return solve(before + inner + after);
-		} else if (md.find()) {
-			Matcher m = md;
-			String before = expr.substring(0, m.start());
-			
-			String first = expr.substring(m.start(1), m.end(1));
-			String second = expr.substring(m.start(2), m.end(2));
-			
-			String inner = String.valueOf(Double.valueOf(first) / Double.valueOf(second));
-			
-			String after = expr.substring(m.end());
-			
-			return solve(before + inner + after);
-		} else if (mp.find()) {
-			Matcher m = mp;
-			String before = expr.substring(0, m.start());
-			
-			String first = expr.substring(m.start(1), m.end(1));
-			String second = expr.substring(m.start(2), m.end(2));
-
-			String inner = String.valueOf(Double.valueOf(first) + Double.valueOf(second));
-			
-			String after = expr.substring(m.end());
-			
-			return solve(before + inner + after);
-		} else if (mm.find()) {
-			Matcher m = mm;
-			String before = expr.substring(0, m.start());
-			
-			String first = expr.substring(m.start(1), m.end(1));
-			String second = expr.substring(m.start(2), m.end(2));
-			
-			String inner = String.valueOf(Double.valueOf(first) - Double.valueOf(second));
-			
-			String after = expr.substring(m.end());
-			
-			return solve(before + inner + after);
-		}
-		return "Error";
+	public Spaceship getShip() {
+		return ship;
 	}
 }
