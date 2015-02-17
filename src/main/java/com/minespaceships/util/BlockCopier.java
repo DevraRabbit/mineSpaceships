9b09c1043c9667d97485ca47d1a9863b52f261f8
Arved Friedemann <arved@uni-bremen.de>
2015-02-17 18:31:32 +0100

    Made some other copying attempts

--- src/main/java/com/minespaceships/util/BlockCopier.java
+++ src/main/java/com/minespaceships/util/BlockCopier.java
@@ -2,6 +2,7 @@ package com.minespaceships.util;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
@@ -9,10 +10,7 @@ import net.minecraft.world.World;
 public class BlockCopier {
 	public static void copyBlock(World worldIn, BlockPos origin, BlockPos target){
 		worldIn.setBlockState(target, worldIn.getBlockState(origin));
-		TileEntity ent = worldIn.getTileEntity(origin);	
-		if(ent != null){
-			worldIn.setTileEntity(target, ent);
-		}
+		removeBlock(worldIn, origin);
 	}
 	public static void removeBlock(World worldIn, BlockPos target){
 		worldIn.setBlockToAir(target);
