a909c0ac81055054af93aad1e7c6f789669baf31
Arved Friedemann <arved@uni-bremen.de>
2015-02-19 19:39:30 +0100

    Tested blocking the updating. Didn't work

--- src/main/java/com/minespaceships/util/BlockCopier.java
+++ src/main/java/com/minespaceships/util/BlockCopier.java
@@ -7,17 +7,20 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldType;
+import net.minecraft.world.chunk.Chunk;
 
 public class BlockCopier {
 	public static void copyBlock(World worldIn, BlockPos origin, BlockPos target){
 		copyBlock(worldIn, origin, target, true);
 	}
 	public static void copyBlock(World worldIn, BlockPos origin, BlockPos target, boolean copyEntity){
-		worldIn.setBlockState(target, worldIn.getBlockState(origin), 2);	
+		setBlockState(target, worldIn.getBlockState(origin), 2, worldIn);	
 		TileEntity ent = worldIn.getTileEntity(origin);
 		if(ent != null){
 			worldIn.removeTileEntity(origin);
@@ -56,4 +59,53 @@ public class BlockCopier {
 	public static void removeBlock(World worldIn, BlockPos target){
 		worldIn.setBlockToAir(target);
 	}
+	public static boolean setBlockState(BlockPos pos, IBlockState newState, int flags, World world)
+    {
+        if (false)//!world.isValid(pos))
+        {
+            return false;
+        }
+        else if (false)//!world.isRemote && world.worldInfo.getTerrainType() == WorldType.DEBUG_WORLD)
+        {
+            return false;
+        }
+        else
+        {
+            Chunk chunk = world.getChunkFromBlockCoords(pos);
+            Block block = newState.getBlock();
+
+            net.minecraftforge.common.util.BlockSnapshot blockSnapshot = null;
+            if (world.captureBlockSnapshots && !world.isRemote)
+            {
+                blockSnapshot = net.minecraftforge.common.util.BlockSnapshot.getBlockSnapshot(world, pos, flags);
+                world.capturedBlockSnapshots.add(blockSnapshot);
+            }
+
+            IBlockState iblockstate1 = chunk.setBlockState(pos, newState);
+
+            if (iblockstate1 == null)
+            {
+                if (blockSnapshot != null) world.capturedBlockSnapshots.remove(blockSnapshot);
+                return false;
+            }
+            else
+            {
+                Block block1 = iblockstate1.getBlock();
+
+                if (block.getLightOpacity() != block1.getLightOpacity() || block.getLightValue() != block1.getLightValue())
+                {
+                	world.theProfiler.startSection("checkLight");
+                	world.checkLight(pos);
+                	world.theProfiler.endSection();
+                }
+
+                if (blockSnapshot == null) // Don't notify clients or update physics while capturing blockstates
+                {
+                	//world.markAndNotifyBlock(pos, chunk, iblockstate1, newState, flags); // Modularize client and physic updates
+                }
+
+                return true;
+            }
+        }
+    }
 }
