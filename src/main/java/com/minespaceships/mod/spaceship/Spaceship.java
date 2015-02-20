b99e3e679543e7312fefba85c10ec0976426e161
Sven Höper <sven@hoeper.me>
2015-02-20 18:30:47 +0100

    optimized moveTo, added JavaDoc
    merged moving and deleting non full blocks into one loop
    added JavaDoc - not finished, would be nice if one could add all
    information

--- src/main/java/com/minespaceships/mod/spaceship/Spaceship.java
+++ src/main/java/com/minespaceships/mod/spaceship/Spaceship.java
@@ -26,18 +26,44 @@ public class Spaceship {
 	private BlockPos maxPosition;
 	private BlockPos span;
 	private BlockPos origin;
-	private WorldServer worldS;
 	
+	/**
+	 * Server world
+	 */
+	private final WorldServer worldS;
 	
-	public Spaceship(final BlockPos minPosition,final BlockPos maxPosition, WorldServer worldS){
+	/**
+	 * Spaceship constructor
+	 * @param minPosition 
+	 * @param maxPosition 
+	 * @param worldS      Server world
+	 */
+	public Spaceship(final BlockPos minPosition,final BlockPos maxPosition, final WorldServer worldS){
 		setMeasurements(minPosition, maxPosition);
 		this.worldS = worldS;
 	}
-	public Spaceship(final BlockPos minPosition, int dimX, int dimY, int dimZ, WorldServer worldS){
+	
+	/**
+	 * Spaceship constructor
+	 * @param minPosition 
+	 * @param dimX        
+	 * @param dimY        
+	 * @param dimZ        
+	 * @param worldS      Server world
+	 */
+	public Spaceship(final BlockPos minPosition, final int dimX, final int dimY, final int dimZ, final WorldServer worldS){
 		BlockPos recSpan = new BlockPos(dimX, dimY, dimZ);
 		setMeasurements(minPosition, ((BlockPos) recSpan).add(minPosition));
 		this.worldS = worldS;
 	}
+	
+	/**
+	 * Spaceship constructor
+	 * @param minSpan 
+	 * @param origin  
+	 * @param maxSpan 
+	 * @param worldS  Server world
+	 */
 	public Spaceship(final BlockPos minSpan, final BlockPos origin, final BlockPos maxSpan, WorldServer worldS){
 		setMeasurements(((BlockPos) minSpan).add(origin), ((BlockPos) maxSpan).add(origin));
 		this.origin = origin;
@@ -50,15 +76,32 @@ public class Spaceship {
 		span = ((BlockPos) maxPos).subtract(minPos);
 		origin = Vec3Op.scale(span, 0.5);
 	}
+	
+	/**
+	 * 
+	 * @param position
+	 */
 	public void setTarget(BlockPos position){
 		moveTo(position.subtract(origin));
 	}
+	
+	/**
+	 * 
+	 * @param addDirection
+	 */
 	public void moveTo(BlockPos addDirection){
-		//copyTo(addDirection, worldC);
 		moveTo(addDirection, worldS);
 	}
+	
+	/**
+	 * move whole spaceship
+	 * @param addDirection 
+	 * @param world        
+	 */
 	private void moveTo(BlockPos addDirection, World world){
 		BlockPos add = new BlockPos(addDirection);
+		
+		// 1. copy all fullcube blocks first (e.g. signs are going to drop otherwise
 		for(int x = 0; x < span.getX(); x++){
 			for(int y = 0; y < span.getY(); y++){
 				for(int z = 0; z < span.getZ(); z++){
@@ -71,6 +114,9 @@ public class Spaceship {
 				}
 			}
 		}
+		
+		// 2. copy all other blocks e.g. signs
+		// 3. delete all non fullcube blocks, so they aren't going to drop
 		for(int x = 0; x < span.getX(); x++){
 			for(int y = 0; y < span.getY(); y++){
 				for(int z = 0; z < span.getZ(); z++){
@@ -79,22 +125,13 @@ public class Spaceship {
 					Block block = world.getBlockState(Pos).getBlock();
 					if(!block.isFullCube()){
 						BlockCopier.copyBlock(world, Pos, Pos.add(add));
-					}
-				}
-			}
-		}
-		for(int x = 0; x < span.getX(); x++){
-			for(int y = 0; y < span.getY(); y++){
-				for(int z = 0; z < span.getZ(); z++){
-					BlockPos Pos = new BlockPos(x,y,z);
-					Pos = Pos.add(minPosition);
-					Block block = world.getBlockState(Pos).getBlock();
-					if(!block.isFullCube()){
 						BlockCopier.removeBlock(world, Pos);
 					}
 				}
 			}
 		}
+		
+		// 4. delete all full blocks
 		for(int x = 0; x < span.getX(); x++){
 			for(int y = 0; y < span.getY(); y++){
 				for(int z = 0; z < span.getZ(); z++){
@@ -107,15 +144,26 @@ public class Spaceship {
 				}
 			}
 		}
+		
 		world.markBlockRangeForRenderUpdate(minPosition, maxPosition);  
 		moveEntities(addDirection);
 		moveMeasurements(addDirection);
 	}
+	
+	/**
+	 * moveMeasurements
+	 * @param addDirection
+	 */
 	private void moveMeasurements(BlockPos addDirection){
 		minPosition = minPosition.add(addDirection);
 		maxPosition = maxPosition.add(addDirection);
 		origin = origin.add(addDirection);
 	}
+	
+	/**
+	 * moveEntities
+	 * @param addDirection
+	 */
 	private void moveEntities(BlockPos addDirection){
 		List<Entity> entities = worldS.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(minPosition, maxPosition));
 		for(Entity ent : entities){			
@@ -127,6 +175,10 @@ public class Spaceship {
 		}
 	}
 
+	/**
+	 * toString
+	 * @return String Spaceship object state information
+	 */
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
