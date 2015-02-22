c997f9cf1d846a7c8206333c5f4754abb53def33
Arved Friedemann <arved@uni-bremen.de>
2015-02-22 21:57:50 +0100

    Made rotational Engines. Only up and Down doesn't work

--- src/main/java/com/minespaceships/mod/blocks/EngineBlock.java
+++ src/main/java/com/minespaceships/mod/blocks/EngineBlock.java
@@ -1,12 +1,19 @@
 package com.minespaceships.mod.blocks;
 
+import java.util.Iterator;
+
+import com.google.common.base.Predicate;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLog;
 import net.minecraft.block.BlockOldLog;
+import net.minecraft.block.BlockPistonBase;
 import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.BlockStairs;
+import net.minecraft.block.BlockTorch;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
@@ -15,12 +22,22 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumWorldBlockLayer;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
-public class EngineBlock extends BlockOldLog {
-
+public class EngineBlock extends Block {
+	public static final PropertyDirection FACING = PropertyDirection.create("facing",  new Predicate()
+    {
+        private static final String __OBFID = "CL_10002054";
+		@Override
+		public boolean apply(Object input) {
+			return input instanceof EnumFacing;
+		}
+    });
+	
 	public EngineBlock() {
-		super();
+		super(Material.iron);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.UP));
 		this.setUnlocalizedName("Engine");
         this.setCreativeTab(CreativeTabs.tabMisc);
 	}
@@ -34,4 +51,131 @@ public class EngineBlock extends BlockOldLog {
 	public EnumWorldBlockLayer getBlockLayer() {
 		return EnumWorldBlockLayer.CUTOUT;
 	}
+	@Override
+	public IBlockState getStateFromMeta(int meta)
+    {
+        IBlockState iblockstate = this.getDefaultState();
+
+        switch (meta)
+        {
+            case 1:
+                iblockstate = iblockstate.withProperty(FACING, EnumFacing.EAST);
+                break;
+            case 2:
+                iblockstate = iblockstate.withProperty(FACING, EnumFacing.WEST);
+                break;
+            case 3:
+                iblockstate = iblockstate.withProperty(FACING, EnumFacing.SOUTH);
+                break;
+            case 4:
+                iblockstate = iblockstate.withProperty(FACING, EnumFacing.NORTH);
+                break;
+            case 5:
+            default:
+                iblockstate = iblockstate.withProperty(FACING, EnumFacing.UP);
+        }
+
+        return iblockstate;
+    }
+	@Override
+	public int getMetaFromState(IBlockState state)
+    {
+        byte b0 = 0;
+        int i;
+
+        switch (EngineBlock.SwitchEnumFacing.FACING_LOOKUP[((EnumFacing)state.getValue(FACING)).ordinal()])
+        {
+            case 1:
+                i = b0 | 1;
+                break;
+            case 2:
+                i = b0 | 2;
+                break;
+            case 3:
+                i = b0 | 3;
+                break;
+            case 4:
+                i = b0 | 4;
+                break;
+            case 5:
+            case 6:
+            default:
+                i = b0 | 5;
+        }
+
+        return i;
+    }
+	static final class SwitchEnumFacing
+    {
+        static final int[] FACING_LOOKUP = new int[EnumFacing.values().length];
+        private static final String __OBFID = "CL_00002053";
+
+        static
+        {
+            try
+            {
+                FACING_LOOKUP[EnumFacing.EAST.ordinal()] = 1;
+            }
+            catch (NoSuchFieldError var6)
+            {
+                ;
+            }
+
+            try
+            {
+                FACING_LOOKUP[EnumFacing.WEST.ordinal()] = 2;
+            }
+            catch (NoSuchFieldError var5)
+            {
+                ;
+            }
+
+            try
+            {
+                FACING_LOOKUP[EnumFacing.SOUTH.ordinal()] = 3;
+            }
+            catch (NoSuchFieldError var4)
+            {
+                ;
+            }
+
+            try
+            {
+                FACING_LOOKUP[EnumFacing.NORTH.ordinal()] = 4;
+            }
+            catch (NoSuchFieldError var3)
+            {
+                ;
+            }
+
+            try
+            {
+                FACING_LOOKUP[EnumFacing.DOWN.ordinal()] = 5;
+            }
+            catch (NoSuchFieldError var2)
+            {
+                ;
+            }
+
+            try
+            {
+                FACING_LOOKUP[EnumFacing.UP.ordinal()] = 6;
+            }
+            catch (NoSuchFieldError var1)
+            {
+                ;
+            }
+        }
+    }
+	
+	@Override
+	public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+        return this.getDefaultState().withProperty(FACING, BlockPistonBase.getFacingFromEntity(worldIn, pos, placer));
+    }
+	@Override
+	protected BlockState createBlockState()
+    {
+        return new BlockState(this, new IProperty[] {FACING});
+    }
 }
