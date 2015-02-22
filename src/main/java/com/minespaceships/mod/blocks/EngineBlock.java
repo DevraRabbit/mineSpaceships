486d3cbeafe3e0e5ed094d60a1c3c9dc9dd65fad
Arved Friedemann <arved@uni-bremen.de>
2015-02-22 19:31:02 +0100

    Made Engines turn. No direction included yet

--- src/main/java/com/minespaceships/mod/blocks/EngineBlock.java
+++ src/main/java/com/minespaceships/mod/blocks/EngineBlock.java
@@ -1,14 +1,26 @@
 package com.minespaceships.mod.blocks;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLog;
+import net.minecraft.block.BlockOldLog;
 import net.minecraft.block.BlockRotatedPillar;
+import net.minecraft.block.BlockStairs;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumWorldBlockLayer;
+import net.minecraft.world.World;
 
-public class EngineBlock extends BlockRotatedPillar {
+public class EngineBlock extends BlockOldLog {
 
 	public EngineBlock() {
-		super(Material.iron);
+		super();
 		this.setUnlocalizedName("Engine");
         this.setCreativeTab(CreativeTabs.tabMisc);
 	}
