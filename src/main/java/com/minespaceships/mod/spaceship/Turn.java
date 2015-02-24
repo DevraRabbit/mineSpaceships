1674f88e1192ddd52a36bca3dddebc748f36db66
Marcel Brannahl <brannahl@uni-bremen.de>
2015-02-24 21:09:35 +0100

    developping ship turns

--- src/main/java/com/minespaceships/mod/spaceship/Turn.java
+++ src/main/java/com/minespaceships/mod/spaceship/Turn.java
@@ -3,7 +3,11 @@ package com.minespaceships.mod.spaceship;
 import com.minespaceships.util.BlockCopier;
 
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockDirectional;
+import net.minecraft.client.Minecraft;
 
 public class Turn {
     public static int LEFT = -1;
@@ -19,19 +23,27 @@ public class Turn {
 					BlockCopier.copyBlock(world, source, target);
 				}
 			}
+		} else {
+			throw new IllegalArgumentException("Invalid argument(s)!");
 		}
 	}
 	
 	public static void around(final World world, final BlockPos origin) {
 		if (world != null && origin != null) {
 			BlockPos source, target;
+			Block targetBlock;
+			boolean hasRotated;
 			for (int x = -10; x <= 10; x++) {
 				for (int z = -10; z <= 10; z++) {
 					source = new BlockPos(origin.getX() + x, origin.getY(), origin.getZ() + z);
 					target = new BlockPos(origin.getX() - x, origin.getY() + 1, origin.getZ() - z);
 					BlockCopier.copyBlock(world, source, target);
+					targetBlock = world.getBlockState(target).getBlock();
+					targetBlock.rotateBlock(world, target, EnumFacing.UP);
 				}
 			}
+		} else {
+			throw new IllegalArgumentException("Invalid argument(s)!");
 		}
 	}
 	
