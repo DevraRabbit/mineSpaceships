cdacba208217ba973e7ff03a985426d61bf2e521
Arved Friedemann <arved@uni-bremen.de>
2015-02-24 14:14:10 +0100

    Made a Map for blocks

--- src/main/java/com/minespaceships/mod/spaceship/SpaceshipMath.java
+++ src/main/java/com/minespaceships/mod/spaceship/SpaceshipMath.java
@@ -1,6 +1,10 @@
 package com.minespaceships.mod.spaceship;
 
+import java.util.ArrayList;
+
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.Vec3i;
+import net.minecraft.world.World;
 
 public class SpaceshipMath {
 	public static boolean isPointInShip(BlockPos pos, Spaceship ship){
@@ -16,4 +20,23 @@ public class SpaceshipMath {
 		}
 		return false;
 	}
+	/*
+	public BlockMap getConnectedPositions(BlockPos pos, World worldIn){
+		BlockMap blockMap = new BlockMap();
+		BlockPos origin = pos;
+		blockMap.addCoordinate(pos.subtract(origin));
+		for(int x = -1; x < 2; x++){
+			for(int y = -1; y < 2; y++){
+				for(int z = -1; z < 2; z++){
+					if(x != 0 && y != 0 && z != 0){
+						BlockPos neighbor = pos.add(x,y,z);
+						if(!blockMap.contains(neighbor) && !worldIn.isAirBlock(neighbor)){
+							blockMap.addCoordinate(neighbor);
+						}
+					}
+				}
+			}
+		}
+	}
+	*/
 }
