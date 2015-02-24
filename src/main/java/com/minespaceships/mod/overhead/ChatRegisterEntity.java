74e3a7c170c00d9aa2a7e3eef4458553beef201d
Jannes Höke <jhoeke@uni-bremen.de>
2015-02-24 17:23:50 +0100

    optimized readability of regexes

--- src/main/java/com/minespaceships/mod/overhead/ChatRegisterEntity.java
+++ src/main/java/com/minespaceships/mod/overhead/ChatRegisterEntity.java
@@ -11,6 +11,7 @@ import com.minespaceships.mod.spaceship.Turn;
 import com.minespaceships.util.BlockCopier;
 import com.minespaceships.util.Calculator;
 import com.minespaceships.util.IMoveable;
+import com.minespaceships.util.PhaserUtils;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiChat;
@@ -30,99 +31,110 @@ import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.Side;
 
-public class ChatRegisterEntity extends TileEntity implements IMoveable{
-	
-	//Attributes
+public class ChatRegisterEntity extends TileEntity implements IMoveable {
+
+	// Attributes
 	private Spaceship ship;
 	private WorldServer remoteWorld;
 
 	private CustomGuiChat terminal;
 	private MenuDisplay menuDisplay;
-	
+
 	private static String recoverSpaceshipMeasures = "recoverSpaceshipMeasurements";
-	
+
 	public ChatRegisterEntity() {
-		remoteWorld = (WorldServer)MinecraftServer.getServer().getEntityWorld();
+		remoteWorld = (WorldServer) MinecraftServer.getServer()
+				.getEntityWorld();
 	}
 
 	@Override
-	public void writeToNBT(NBTTagCompound par1)
-	{
-	   super.writeToNBT(par1);
-	   if(ship != null){
-		   par1.setIntArray(recoverSpaceshipMeasures, ship.getOriginMeasurementArray());
-	   }	   
+	public void writeToNBT(NBTTagCompound par1) {
+		super.writeToNBT(par1);
+		if (ship != null) {
+			par1.setIntArray(recoverSpaceshipMeasures,
+					ship.getOriginMeasurementArray());
+		}
 	}
 
 	@Override
-	public void readFromNBT(NBTTagCompound par1)
-	{
-	   super.readFromNBT(par1);
-	   int[] measurements = par1.getIntArray(recoverSpaceshipMeasures);
-	   if(measurements != null){
-		   ship = new Spaceship(measurements);
-	   }
+	public void readFromNBT(NBTTagCompound par1) {
+		super.readFromNBT(par1);
+		int[] measurements = par1.getIntArray(recoverSpaceshipMeasures);
+		if (measurements != null) {
+			ship = new Spaceship(measurements);
+		}
 	}
 
 	/**
 	 * Activates the TileEntity and opens a custom chat to the player
+	 * 
 	 * @param player
 	 */
-	public void Activate(EntityPlayer player){
-		//check if the player is our local player, so one cannot open a console for another player
-		//on the server
-		if(player.equals(Minecraft.getMinecraft().thePlayer)){
-			//initialise the terminal
-			//terminal = new CustomGuiChat(player, (ChatRegisterEntity)remoteWorld.getTileEntity(pos));
+	public void Activate(EntityPlayer player) {
+		// check if the player is our local player, so one cannot open a console
+		// for another player
+		// on the server
+		if (player.equals(Minecraft.getMinecraft().thePlayer)) {
+			// initialise the terminal
+			// terminal = new CustomGuiChat(player,
+			// (ChatRegisterEntity)remoteWorld.getTileEntity(pos));
 			terminal = new CustomGuiChat(player, this);
-			
-			//Initialise a default menu for testing reasons
-			if(!DefaultMenu.getRunBefore()){
+
+			// Initialise a default menu for testing reasons
+			if (!DefaultMenu.getRunBefore()) {
 				DefaultMenu.initMenu(terminal);
 			}
 
-			//initialise the menu display.
+			// initialise the menu display.
 			menuDisplay = new MenuDisplay(DefaultMenu.getRootMenu(), terminal);
 
-			//open our console. 
+			// open our console.
 			Minecraft.getMinecraft().displayGuiScreen(terminal);
-			//Print out the menu in the console.
+			// Print out the menu in the console.
 			menuDisplay.displayMain(DefaultMenu.getRootMenu());
 		}
 	}
-	public void setRemoteWorld(WorldServer world){
+
+	public void setRemoteWorld(WorldServer world) {
 		remoteWorld = world;
 	}
+
 	/**
 	 * Executes the given command, regardless who committed it.
+	 * 
 	 * @param command
 	 */
-	public void onCommand(String command){
-		
+	public void onCommand(String command) {
+
 	}
+
 	/**
 	 * Executing the given command considering the player that sent it.
+	 * 
 	 * @param command
 	 * @param player
 	 */
-	public void onCommand(String command, EntityPlayer player){
-		//display the menu.
+	public void onCommand(String command, EntityPlayer player) {
+		// display the menu.
 		menuDisplay.display(command);
 
-		//define a very first command to see if it works.
-		if(command.equals("hello")){
-			//send something to the player to see if we get a feedback from our command.
+		// define a very first command to see if it works.
+		if (command.equals("hello")) {
+			// send something to the player to see if we get a feedback from our
+			// command.
 			player.addChatComponentMessage(new ChatComponentText("I love you!"));
-		//Define the 'calc' command, which parses a math expression
-		} else if(command.startsWith("calc")) {
+			// Define the 'calc' command, which parses a math expression
+		} else if (command.startsWith("calc")) {
 			Calculator.calc(command, player);
 		} else if (command.startsWith("init")) {
 			SpaceshipCommands.init(command, remoteWorld, this, player, ship);
 		} else if (command.startsWith("move")) {
 			SpaceshipCommands.move(command, remoteWorld, this, player, ship);
 		} else if (command.equals("test1")) {
-			SpaceshipCommands.init("init -4;-4;-4 to 4;4;4", remoteWorld, this, player, ship);
-			SpaceshipCommands.move("move 0;15;0", remoteWorld, this, player, ship);
+			SpaceshipCommands.init("init -4;-4;-4 to 4;4;4", remoteWorld, this,
+					player, ship);
+			SpaceshipCommands.move("move 0;15;0", remoteWorld, this, player,
+					ship);
 		} else if (command.startsWith("turn ")) {
 			command = command.substring(4).trim();
 			if (command.equals("left")) {
@@ -132,10 +144,31 @@ public class ChatRegisterEntity extends TileEntity implements IMoveable{
 			} else if (command.equals("around")) {
 				Turn.around(worldObj, pos);
 			} else {
-				player.addChatComponentMessage(new ChatComponentText("Invalid direction! Only left, right or around!"));
+				player.addChatComponentMessage(new ChatComponentText(
+						"Invalid direction! Only left, right or around!"));
 			}
-		} else if(command.equals("status")) {
+		} else if (command.equals("status")) {
 			SpaceshipCommands.status(remoteWorld, this, player, ship);
+		} else if (command.startsWith("phaser")) {
+
+			// TODO: Move this to an appropriate class
+			command = command.substring("phaser".length())
+					.replaceAll("\\s", "");
+			Pattern poffset = Pattern
+					.compile("([-+]?[0-9]*\\.?[0-9]+);([-+]?[0-9]*\\.?[0-9]+);([-+]?[0-9]*\\.?[0-9]+)");
+			Matcher moffset = poffset.matcher(command);
+
+			if (moffset.matches()) {
+				Vec3 direction = new Vec3(Double.valueOf(moffset.group(1)),
+						Double.valueOf(moffset.group(2)),
+						Double.valueOf(moffset.group(3)));
+
+				player.addChatComponentMessage(new ChatComponentText("Test-shooting Phaser with 10 strength and 100 max range."));
+				PhaserUtils.shoot(pos, direction, 10, 100, remoteWorld);
+			} else {
+				player.addChatComponentMessage(new ChatComponentText("Input error. Usage: phaser #;#;#"));
+			}
+
 		}
 		terminalMenu.onCommand(command, player);
 	}
@@ -143,14 +176,15 @@ public class ChatRegisterEntity extends TileEntity implements IMoveable{
 	public void setShip(Spaceship ship) {
 		this.ship = ship;
 	}
-	
+
 	public Spaceship getShip() {
 		return ship;
 	}
+
 	@Override
 	public void moveInformation(IMoveable target) {
-		if(target instanceof ChatRegisterEntity){
-			ChatRegisterEntity targetEntity = (ChatRegisterEntity)target;
+		if (target instanceof ChatRegisterEntity) {
+			ChatRegisterEntity targetEntity = (ChatRegisterEntity) target;
 			targetEntity.ship = ship;
 			targetEntity.remoteWorld = remoteWorld;
 		}
