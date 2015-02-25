f5e6f337ce45c996807cfe4ced07f2f914eba72d
Sven Höper <sven@hoeper.me>
2015-02-25 16:22:21 +0100

    onExplosionEvent handling, comments for BlockEvent
    on explosion now calls Shipyard.blockBroken for each affected block
    added short comments on which event does what

--- src/main/java/com/minespaceships/mod/events/BlockEvent.java
+++ src/main/java/com/minespaceships/mod/events/BlockEvent.java
@@ -1,11 +1,16 @@
 package com.minespaceships.mod.events;
 
+import java.util.List;
+
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.event.world.BlockEvent.MultiPlaceEvent;
 import net.minecraftforge.event.world.BlockEvent.PlaceEvent;
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.eventhandler.Event.HasResult;
 
 import com.minespaceships.mod.spaceship.Shipyard;
 
@@ -22,6 +27,11 @@ public class BlockEvent {
 	// Minecraft.getMinecraft().player.sendMessage
 	// BreakEvent
 	// http://www.minecraftforge.net/wiki/Event_Reference#BreakEvent
+	
+	/*
+	 * Calls blockBroken for placed block,
+	 * to remove the block from ships block list if member of a ship
+	 */
 	@SubscribeEvent
 	public void onBreakEvent(final BreakEvent event) {
 		if (event.isCanceled()) return;
@@ -31,6 +41,10 @@ public class BlockEvent {
 		Shipyard.getShipyard().blockBroken(pos, world);
 	}
 	
+	/*
+	 * Calls blockPlaced to add block to ships block list if placed
+	 * block is next to a ship
+	 */
 	@SubscribeEvent
 	public void onPlaceEvent(final PlaceEvent event) {
 		if (event.isCanceled()) return;	
@@ -40,7 +54,11 @@ public class BlockEvent {
 		Shipyard.getShipyard().blockPlaced(pos, world);
 	}
 	
-	// see http://www.minecraftforge.net/forum/index.php?topic=24376.0
+	 /*
+	  * Calls blockPlaced to add block to ships block list if placed
+	  * block is next to a ship
+	  * see http://www.minecraftforge.net/forum/index.php?topic=24376.0
+	  */
 	@SubscribeEvent
 	public void onMultiPlaceEvent(final MultiPlaceEvent event) {
 		if (event.isCanceled()) return;
@@ -48,5 +66,19 @@ public class BlockEvent {
 		BlockPos pos = event.pos;
 
 		Shipyard.getShipyard().blockPlaced(pos, world);
+	}
+	
+	/*
+	 * Calls blockBroken for each block affected by explosion,
+	 * to remove the blocks from ships block list if member of a ship
+	 */
+	@SubscribeEvent
+	public void onExplosionEvent(final ExplosionEvent.Detonate event) {
+		if (event.isCanceled()) return;
+		World world = event.world;
+		List<BlockPos> posList = event.getAffectedBlocks();
+		for(BlockPos pos: posList) {
+			Shipyard.getShipyard().blockBroken(pos, world);
+		}
 	}
 }
